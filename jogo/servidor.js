6ccc7ddffd20899cb058b8964b41a8bfe0906069
Bernardo121431 <160230915+Bernardo121431@users.noreply.github.com>
2024-03-14 20:49:04 -0300

    Delete jogo directory

--- jogo/servidor.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
-
-app.use(express.static('cliente/'))
-app.listen(PORT, () => console.log(`Servidor em execução na porta ${PORT}!`))
-
